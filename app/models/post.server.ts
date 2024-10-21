f4f386b00b38de2ee11656b8deb07b90cb0c0614
Roger Lam <rogerlam@Rogers-MacBook-Pro.local>
2024-10-21 10:07:58 -0400

    Use the supabase client to get posts table

--- app/models/post.server.ts
+++ app/models/post.server.ts
@@ -1,9 +1,21 @@
+import { supabase } from "~/supabase";
+
 type Post = {
   slug: string;
   title: string;
 };
 
 export async function getPosts(): Promise<Array<Post>> {
+  const { data, error } = await supabase
+    .from("posts")
+    .select("*");
+
+  if (error) {
+    console.error('Error fetching posts:', error);
+  } else {
+    console.log(data);
+  }
+
   return [
     {
       slug: "my-first-post",
